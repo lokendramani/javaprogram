ec82e580a59d24b7476351fb283d350f8096b605
lokendramani <lokendramani@gmail.com>
2021-12-16 19:28:26 +0530

    Armstrong number

---
+++ src/main/java/org/example/Armstrong.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import com.sun.org.apache.xerces.internal.impl.xs.SchemaNamespaceSupport;
+
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        int n = s.nextInt();
+        int num = n;
+        int digit=0;
+        int mod = 0;
+        int sum=0;
+        while(num > 0){
+            digit++;
+            num/=10;
+        }
+        num = n;
+        while(num > 0){
+            mod = num % 10;
+            sum += Math.pow(mod,digit);
+            num/=10;
+        }
+
+        if (sum == n ) {
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
+}
