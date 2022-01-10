c5d0b499d00230d8c36549ee51dd7f399c46443c
lokendramani <lokendramani@gmail.com>
2022-01-10 20:19:19 +0530

    Added LeaderElementsInArray

---
+++ src/main/java/org/example/LeaderElementsInArray.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class LeaderElementsInArray {
+    public static void main(String[] args) {
+        int[] input = {13,17,5,4,6};
+        leaders(input);
+    }
+
+    private static void leaders(int[] input) {
+        boolean leaderFound = false;
+        for (int i = 0; i < input.length; i++) {
+            leaderFound = true;
+            for (int j = i; j < input.length ; j++) {
+                if(input[i] < input[j]){
+                    leaderFound = false;
+                    break;
+                }
+            }
+            if(leaderFound == true){
+                System.out.print(input[i]+" ");
+            }
+        }
+    }
+}
